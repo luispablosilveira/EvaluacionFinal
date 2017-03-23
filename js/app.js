ae33225654f291b358601e1e04d131c4e98d2ed0
= <=>
2017-03-23 06:49:40 -0300

    Cambios al 2017-03-22
    Cambios al 2017-03-22

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 var display = document.getElementById("display");
 
-var numeroUno;
-ver numeroDos;
-var resultado;
+var numeroUno					= "";
+var operadorEjecutado	= "";
+var numeroDos					= "";
+var resultado					= "";
+var ultimoDigitado		= "";
 
 function reducirTecla(elemento){
 	elemento.setAttribute("style", "transform:scale(0.95,0.95)")
@@ -34,40 +36,56 @@ function procesarTecla(elemento){
 			break;
 
 		case 'menos':
-		case 'mas':
-		case 'por':
-		case 'dividido':
-			alert("Operador");
+			operador("-");
 			break;
 
+		case 'mas':
+			operador("+");
+			break;
+
+		case 'por':
+			operador("*");
+			break;
+
+		case 'dividido':
+			operador("/");
+			break;
 
 		case 'punto':
-				punto();
-				break;
+			punto();
+			break;
 
 		case 'igual':
-			alert("igual");
+			igual();
 			break;
 
 		default:
 			break;
 	}
+
+	ultimoDigitado = elemento.id;
 }
 function numero(valor){
+		// Si es 0 cargo el valor
 		if (display.innerHTML == "0"){
 			display.innerHTML = valor;
 		}else{
-			display.innerHTML += valor;
+			// Si no es 0, me fijo que el largo no sea superior a 8
+			if (display.innerHTML.length < 8){
+				display.innerHTML += valor;
+			}
 		}
 }
 function on(){
-	numeroUno = 0;
-	numeroDos = 0;
-	resultado = 0;
+	inicializoValores();
 	display.innerHTML = "0";
 }
 function punto(){
-	
+	// Si no tiene punto y el largo es menor que 8 agrego el punto
+	alert(display.innerHTML.indexOf("."));
+	if (display.innerHTML.indexOf(".") == -1){
+		display.innerHTML += ".";
+	}
 }
 function signo(){
 	if (display.innerHTML != "0"){
@@ -79,6 +97,68 @@ function signo(){
 		}
 	}
 }
+function operador(oper){
+	// No se puede haber ejecutado un operador anteriormente
+	if (operadorEjecutado == ""){
+		numeroUno = display.innerHTML;
+		operadorEjecutado = oper;
+		display.innerHTML = "";
+	}
+	else{
+		alert("Esta digitando el segundo numero, solo se acepta el igual o limpiar");
+	}
+}
+function igual(){
+	if (ultimoDigitado == "igual"){
+			calcularResultado();
+	}else{
+		if (numeroDos == ""){
+			numeroDos = display.innerHTML;
+		}
+
+		calcularResultado();
+	}
+
+
+	display.innerHTML = resultado;
+
+
+}
+function calcularResultado(){
+	switch(operadorEjecutado){
+		case '+':
+			resultado = parseFloat(numeroUno) + parseFloat(numeroDos);
+			break;
+
+		case '-':
+			resultado = parseFloat(numeroUno) - parseFloat(numeroDos);
+			break;
+
+		case '*':
+			resultado = parseFloat(numeroUno) * parseFloat(numeroDos);
+			break;
+
+	  case '/':
+			if (numeroDos != "0"){
+				resultado = parseFloat(numeroUno) / parseFloat(numeroDos);
+			}else{
+				alert("No se puede dividir entre 0");
+			}
+			break;
+
+		default:
+			break;
+	}
+
+	numeroUno = resultado;
+}
+function inicializoValores(){
+	numeroUno = "";
+	numeroDos = "";
+	operador 	= "";
+	resultado = "";
+}
+
 var Calculadora = {
 	init: function(){
 
@@ -128,6 +208,7 @@ var Calculadora = {
 		document.getElementById("igual").onclick 		= this.procesoTecla;
 
 	}
+
 }
 
 Calculadora.init();
